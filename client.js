68b68a93ff1397f5c4f3c9d927b28bec4d7b4a01
Kauan Pardini Augsuto <kauanpardini@gmail.com>
2023-11-04 07:17:39 -0300

    asdsa
    Criado distinção de mensagem

--- client.js
+++ client.js
@@ -1,2 +1,42 @@
 const socket = io();
 
+const form = document.getElementById('form');
+const input = document.querySelector('.input');
+const campoMensagem = document.getElementById('campo-mensagem');
+
+const maxMessageHistory = 2;
+
+socket.on('message history', (history) => {
+    // Exibir o histórico de mensagens para o novo usuário
+
+    const startIndex = Math.max(history.length - maxMessageHistory, 0);
+    const limitedHistory = history.slice(startIndex);
+
+    limitedHistory.forEach((msg) => {
+        const item = document.createElement('li');
+        item.textContent = msg;
+        campoMensagem.classList.add('active');
+        campoMensagem.appendChild(item);
+    });
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (input.value) {
+        socket.emit('chat message', input.value);
+        input.value = '';
+    }
+});
+
+socket.on('chat message', (msg) => {
+    const item = document.createElement('li');
+    item.textContent = msg;
+    campoMensagem.appendChild(item);
+    campoMensagem.classList.add('active');
+    campoMensagem.scrollTo(0, document.body.scrollHeight);
+
+    history.push(msg);
+    if (history.length > maxMessageHistory) {
+        history.shift(); // Remover a mensagem mais antiga
+    }
+});
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+const messages = [];
+
 // Lista de arquivos estáticos que você deseja servir
 const staticFiles = [
     'assets/mononoke.jpg',
@@ -17,23 +19,31 @@ const staticFiles = [
 
 app.get('/assets/apple-touch-icon.png', (req, res) => {
     res.sendFile(join(__dirname + '/assets/apple-touch-icon.png'));
-    console.log("deu certo");
 });
 
 app.get('/assets/favicon-32x32.png', (req, res) => {
     res.sendFile(join(__dirname + '/assets/favicon-32x32.png'));
-    console.log("deu certo");
 });
 
 app.get('/assets/favicon-16x16.png', (req, res) => {
     res.sendFile(join(__dirname + '/assets/favicon-16x16.png'));
-    console.log("deu certo");
 });
 
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+
+        messages.push(msg);
+
+        io.emit('chat message', msg);
+    });
+
+    socket.emit('message history', messages);
+
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
@@ -42,3 +52,5 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
+
+io.emit('hello', 'world'); 
